8b622abbcb229a33479658d01441e745c95759f5
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2025-08-10 21:49:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kk71285631:KWuoAmrRouFPkTYB@cluster0.yvg41g8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bro096316:MnX37xWMlGYLYCmM@cluster0.mdi3pod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Abhi72")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
