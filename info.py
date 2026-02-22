7252166a7e129ace186ccd27d8ac0508c1ea74e7
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-02-22 19:29:19 +0530

    Change MongoDB connection details
    Updated MongoDB connection details for DATABASE_URI and DATABASE_NAME.

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bro096316:MnX37xWMlGYLYCmM@cluster0.mdi3pod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "bro096316)
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ABHISHEK:ABHISHEK@cluster0.0r8bnmx.mongodb.net/?appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "ABHISHEK)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
