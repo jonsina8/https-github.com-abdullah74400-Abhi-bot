91edcead774514ee4a8720f5d48d9b35d118e53b
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2026-02-23 10:26:05 +0530

    Add new admin ID to the ADMINS list

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/uxS.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/uxB.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1807927334 7436687322').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1807927334 7436687322 6765929934').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002520793005').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
